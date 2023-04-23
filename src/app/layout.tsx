af86d233dfd894b1bfa20f8350687fad1fca9f16
Enoch <mmejuenoch@gmail.com>
2023-04-23 20:19:58 +0100

    Learnt about the next google fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import { Poppins } from "next/font/google";
 import "@/style/globals.css";
 
 export const metadata = {
@@ -7,14 +8,20 @@ export const metadata = {
   keywords: "nextjs, nextjs13, nextjs-13, typescript, nodejs, node",
 };
 
+const pops = Poppins({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-main",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="h-screen bg-gray-900 text-gray-200">
+    <html lang="en" className={`${pops.variable}`}>
+      <body className={`h-screen bg-gray-900 text-gray-200 font-main`}>
         <Header />
         <main className="p-4">{children}</main>
       </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        main: ["var(--font-main)"],
+      },
+    },
   },
   plugins: [],
 };
